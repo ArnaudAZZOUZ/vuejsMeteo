05a39180ce68075408449587221732ce51812692
Arnaud <arnaud.azzouz-thuderoz@le-campus-numerique.fr>
2021-02-25 11:01:31 +0100

    store ok

--- src/components/CitiesList.vue
+++ src/components/CitiesList.vue
@@ -6,26 +6,59 @@
 </template>
 
 <script lang="ts">
-  import City from "./City.vue";
-  import axios from "axios";
-  import { defineComponent } from 'vue';
-  // import store from '../store/store';
-  import { useStore } from '@/store/store';
+import City from "./City.vue";
+import { defineComponent, computed } from 'vue';
+import { useStore } from 'vuex';
 
+export default defineComponent ({
+  name: 'CitiesListStore',
+  components: {
+    City
+  },
+  setup () {
+    const store = useStore();
 
-  export default defineComponent ({
-    name: 'CitiesList',
-    components: {
-      City
-    },
-  computed:{
-      useStore.this.state.
+    store.dispatch('getCitiesAsync');
+    return {
+      cities: computed( () => store.state.cities)
+    }
   }
-  })
+})
 </script>
 
 <style scoped>
-  h1 {
-    margin: 40px 0 0;
-  }
+h1 {
+  margin: 40px 0 0;
+}
 </style>
+<!--<template>-->
+<!--  <div>-->
+<!--    <h1>Liste des villes</h1>-->
+<!--    <City v-for="city of cities" :key="city.id" :name="city.name" :weather="city.weather" :temperature="city.temperature" :updated-at="city.updatedAt"></City>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script lang="ts">-->
+<!--  import City from "./City.vue";-->
+<!--  import axios from "axios";-->
+<!--  import { defineComponent } from 'vue';-->
+<!--  // import store from '../store/store';-->
+<!--  import { useStore } from '@/store/store';-->
+
+
+<!--  export default defineComponent ({-->
+<!--    name: 'CitiesList',-->
+<!--    components: {-->
+<!--      City-->
+<!--    },-->
+<!--  computed:{-->
+<!--      useStore.this.state.-->
+<!--  }-->
+<!--  })-->
+<!--</script>-->
+
+<!--<style scoped>-->
+<!--  h1 {-->
+<!--    margin: 40px 0 0;-->
+<!--  }-->
+<!--</style>-->
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,35 +1,42 @@
-import {createStore} from "vuex"
+import { createStore } from 'vuex'
 import axios from "axios";
 
 export interface State {
-    cities: Array<Object>
+    cities: any;
+    iconWidth: Number;
+    iconHeight: Number;
 }
 
 export const store = createStore<State>({
 
-    state: {
-        cities: []
-    },
-
-    mutations: {
-        loadCities(state,cities): void {
-
-            for (const {
-                state,
-                weather: [{description: weather}],
-                main: {temp: temperature},
-                dt: updatedAt
-            } of cities) {
-                state.cities.push({name, weather, temperature, updatedAt: new Date(updatedAt * 1000)});
+    actions: {
+        getCitiesAsync ({ commit, state }) {
+            if (localStorage.getItem('cities') == null) {
+                console.log('CALL API')
+                axios
+                    .get('https://api.openweathermap.org/data/2.5/find?lat=' + process.env.VUE_APP_DEFAULT_LATITUDE + '&lon=' + process.env.VUE_APP_DEFAULT_LONGITUDE + '&cnt=20&cluster=yes&lang=fr&units=metric&APPID=' + process.env.VUE_APP_OW_APP_ID)
+                    .then((citiesData) => {
+                        let cities = [];
+                        for (const { name, coord: {lat, lon}, weather: [{description: weather, icon: icon}], main: {temp: temperature}, dt: updatedAt } of citiesData.data.list) {
+                            cities.push({ name, lat, lon, weather, icon, temperature, updatedAt: new Date(updatedAt) });
+                        }
+                        commit('SaveCities', cities);
+                        localStorage.setItem('cities', JSON.stringify(cities)); //backup
+                    })
+            } else {
+                console.log('PAS CALL API');
+                commit('SaveCities', JSON.parse(localStorage.cities));
             }
         }
     },
-    // action: {
-    //     axios
-    //     .get(`https://api.openweathermap.org/data/2.5/find?lat=${process.env.VUE_APP_DEFAULT_LATITUDE}&lon=${process.env.VUE_APP_DEFAULT_LONGITUDE}&cnt=20&cluster=yes&lang=fr&units=metric&APPID=${process.env.VUE_APP_OW_APP_ID}`)
-    //         .then((resp) => this.loadCities(resp.data.list));
-    // }
-
+    mutations: {
+        SaveCities(state, citiesData){
+            state.cities = citiesData;
+        },
+    },
+    state: {
+        cities: [],
+        iconWidth: 50,
+        iconHeight: 50
+    },
 })
-
-export default store
